dc49e7a0256e9ccbb32ec8b30dfc82ad4023b718
General Kroll <generalkroll0@gmail.com>
2022-09-19 12:05:05 +1000

    sqlean-functions
    Summary:
    
    - Adding **some** functionality from sqlean, acknowledged appropriately in code comments.
    - Amendments as required to avoid compiler errors and warnings.

---
+++ sqlite3_opt_sqleanall.go
@@ -0,0 +1,14 @@
+// Copyright (C) 2019 Yasuhiro Matsumoto <mattn.jp@gmail.com>.
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
+//go:build sqlite_sqleanall || sqleanall
+// +build sqlite_sqleanall sqleanall
+
+package sqlite3
+
+/*
+#cgo CFLAGS: -DSQLITE_ENABLE_SQLEANALL -DSQLITE_ENABLE_UNICODE
+*/
+import "C"
